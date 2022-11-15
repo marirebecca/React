d81c8f03b9d740f8d9b58facb58eb82103bf05dc
marirebecca <mari_rebecca@hotmail.com>
2022-11-15 12:50:00 -0300

    Postagem e Tema

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home'
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 
 function App() {
   return (
-  
-      <Router>
-        <Navbar />
-          <div style={{minHeight: '100vh'}}>
-            <Routes> // Antigo Switch
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-            </Routes>
 
-            </div>
-        <Footer />
-      </Router>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
+
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
