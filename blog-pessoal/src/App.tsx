d4cdd492f420340e9e88d83643d991b8a6862e8e
marirebecca <mari_rebecca@hotmail.com>
2022-11-14 22:08:02 -0300

    Blog-Pessoal

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home'
+import './App.css';
+
+function App() {
+  return (
+  
+      <>
+        <Navbar />
+          
+                <Home />
+              
+        <Footer />
+      </>      
+  );
+}
+
+export default App;
---
+++ blog-pessoal/src/paginas/login/Login.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Grid, Box} from '@material-ui/core'
+import './Login.css';
+
+function Login() {
+
+    return (
+        <Grid container>
+            <Grid xs={6}>
+                <Box>
+                    <form>
+
+                    </form>
+                </Box>
+            </Grid>
+
+            <Grid xs={6}>
+
+            </Grid>
+        </Grid>
+    );
+
+}
+
+export default Login;
