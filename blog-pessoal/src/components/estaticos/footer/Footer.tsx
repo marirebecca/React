dd04603ba53ebb77c2a210fee05d3747b7a02730
marirebecca <mari_rebecca@hotmail.com>
2022-11-15 23:59:55 -0300

    Update Footer.tsx

--- blog-pessoal/src/components/estaticos/footer/Footer.tsx
+++ blog-pessoal/src/components/estaticos/footer/Footer.tsx
@@ -3,7 +3,7 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
-import { CFooter } from '@coreui/react'
+import { footerComponent } from '@coreui/react'
 
 function Footer() {
     return (
@@ -14,7 +14,7 @@ function Footer() {
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
-                        <CFooter>
+                        <footerComponent>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
                                 <FacebookIcon style={{ fontSize: 60, color: "white" }} />
@@ -26,7 +26,7 @@ function Footer() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
                             </a>
                         </Box>
-                        </CFooter>
+                        </footerComponent>
                     </Box>
                     <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
                         <Box paddingTop={1}>
